497b505e69abdddcf481e02604569c7285624b65
leejungho <ljhmoa@gmail.com>
2023-03-02 14:50:19 +0900

    [Update] 화살표 함수 학습

--- my-app/src/ES/arrowFunction.js
+++ my-app/src/ES/arrowFunction.js
@@ -11,9 +11,12 @@ const sleep1 = function (repeatCount = 1) {
 // sleep 화살표 함수 식으로 작성합니다.
 
 const sleep = (repeatCount = 1) => {
-  for (let i; i < repeatCount; i++) {
+  while (repeatCount--) {
     console.log("졸려요!");
   }
+  // for (let i; i < repeatCount; i++) {
+  //   console.log("졸려요!");
+  // }
 };
 
 sleep(10);
